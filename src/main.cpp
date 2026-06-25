140431731770533a5efa54badf0d115d97616eea
le10x <leanderdrb@gmail.com>
2026-06-25 13:08:52 -0400

    Modify pause layer and game object collision logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,50 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/modify/GameObject.hpp>
+#include <cstdlib>
+#include <ctime>
 
 using namespace geode::prelude;
 
 bool g_suerteDecidida = false;
 bool g_noclipSecreto = false;
 
-// 1. AÑADIR EL BOTÓN DE PAUSA
 class $modify(MyPauseLayer, PauseLayer) {
     void customSetup() {
         PauseLayer::customSetup();
-        auto rightMenu = this->getChildByID("right-button-menu");
-        if (!rightMenu) return;
+        auto menu = this->getChildByID("right-button-menu");
+        if (!menu) return;
 
         auto btn = CCMenuItemSpriteExtra::create(
             ButtonSprite::create("¿?", "goldFont.fnt", "GJ_button_02.png", 0.7f),
             this, menu_selector(MyPauseLayer::alPresionar)
         );
-        rightMenu->addChild(btn);
-        rightMenu->updateLayout();
+        menu->addChild(btn);
+        menu->updateLayout();
     }
 
     void alPresionar(CCObject*) {
+        std::srand(static_cast<unsigned int>(std::time(nullptr))); // Semilla del azar
         g_noclipSecreto = (std::rand() % 2 == 1);
         g_suerteDecidida = true;
-        FLAlertLayer::create("Destino Sellado", "El azar ha decidido tu suerte en secreto. <y>Averígualo jugando...</y>", "¡Ok!") -> show();
+        FLAlertLayer::create("Destino Sellado", "El azar decidió tu suerte en secreto. <y>Averígualo jugando...</y>", "¡Ok!") -> show();
     }
 };
 
-// 2. LÓGICA DEL NOCLIP SECRETO (Evita errores de funciones inline)
 class $modify(GameObject) {
-    bool collideWithPlayer(PlayerObject* player) {
+    // Añadimos 'GJCollideInfo*' que es requerido en Geometry Dash 2.2
+    bool collideWithPlayer(PlayerObject* player, GJCollideInfo* info) {
         if (g_suerteDecidida) {
-            g_suerteDecidida = false; // Solo avisa en el primer choque
+            g_suerteDecidida = false; 
             if (g_noclipSecreto) {
                 Notification::create("¡BENDITO POR EL AZAR! (Noclip Activo)", NotificationIcon::Success, 1.5f) -> show();
             } else {
                 Notification::create("¡EL DESTINO TE ABANDONÓ!", NotificationIcon::Error, 2.0f) -> show();
             }
         }
-        // Si ganaste el 50% secreto, cancelamos la colisión regresando falso
         if (g_noclipSecreto) return false;
         
-        return GameObject::collideWithPlayer(player);
+        // Pasamos ambos parámetros a la función original del juego
+        return GameObject::collideWithPlayer(player, info);
     }
 };
