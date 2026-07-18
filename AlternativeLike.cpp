04afdbaaee92e96a4e711223c55732177676648f
le10x <leanderdrb@gmail.com>
2026-07-18 16:15:35 -0400

    Rename MyLevelInfoLayer to LikeLayer and update logic

--- AlternativeLike.cpp
+++ AlternativeLike.cpp
@@ -3,30 +3,25 @@
 
 using namespace geode::prelude;
 
-class $modify(MyLevelInfoLayer, LevelInfoLayer) {
+class $modify(LikeLayer, LevelInfoLayer) {
     bool init(GJGameLevel* level, bool p1) {
         if (!LevelInfoLayer::init(level, p1)) return false;
 
         if (auto menu = this->getChildByID("left-side-menu")) {
-            
-            // Creamos el sprite directamente usando la textura nativa del juego
             auto btnSprite = CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png");
             
             if (btnSprite) {
-                // Creamos el botón interactivo usando el sprite limpio, sin contenedores extra
                 auto myButton = CCMenuItemSpriteExtra::create(
                     btnSprite,
                     this,
-                    menu_selector(MyLevelInfoLayer::onAlternativeLike)
+                    menu_selector(LikeLayer::onAlternativeLike)
                 );
 
                 myButton->setID("alt-like-button");
-
                 menu->addChild(myButton);
                 menu->updateLayout();
             }
         }
-
         return true;
     }
 
@@ -38,7 +33,6 @@ class $modify(MyLevelInfoLayer, LevelInfoLayer) {
                 true, 
                 0
             );
-
             FLAlertLayer::create("Geode", "¡Like enviado!", "OK")->show();
         }
     }
