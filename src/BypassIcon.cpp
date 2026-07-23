d315ae67210de15e1da3782e3c6cd49b54977ca1
le10x <leanderdrb@gmail.com>
2026-07-23 14:06:23 -0400

    Refactor bypass logic for icon and color unlocking

--- src/BypassIcon.cpp
+++ src/BypassIcon.cpp
@@ -6,29 +6,20 @@ using namespace geode::prelude;
 class $modify(GameManager) {
 
     bool isIconUnlocked(int iconID, IconType iconType) {
-        bool bypassActive = Mod::get()->getSettingValue<bool>("bypass-icons");
+        bool bypassIconActive = Mod::get()->getSettingValue<bool>("bypass-icons");
         
-        if (bypassActive) {
+        if (bypassIconActive) {
             return true;
         }
         return GameManager::isIconUnlocked(iconID, iconType);
     }
 
     bool isColorUnlocked(int colorID, UnlockType unlockType) {
-        bool bypassActive = Mod::get()->getSettingValue<bool>("bypass-icons");
+        bool bypassIconActive = Mod::get()->getSettingValue<bool>("bypass-icons");
         
-        if (bypassActive) {
+        if (bypassIconActive) {
             return true;
         }
         return GameManager::isColorUnlocked(colorID, unlockType);
-    }
-
-    bool hasItemUnlocked(UnlockType unlockType, int itemID) {
-        bool bypassActive = Mod::get()->getSettingValue<bool>("bypass-icons");
-        
-        if (bypassActive) {
-            return true;
-        }
-        return GameManager::hasItemUnlocked(unlockType, itemID);
     }
 };
